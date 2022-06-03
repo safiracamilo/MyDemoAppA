f4e5f1c08d7629703361461a6cb08610eba7b7a2
Safira <safiracamilo@gmail.com>
2022-06-03 16:38:50 +0100

    Mydemoaap Sauce Lab e Localmente

---
+++ tests/simples.py
@@ -0,0 +1,99 @@
+# This sample code uses the Appium python client v2
+# pip install Appium-Python-Client
+# Then you can paste this into a file and simply run with Python
+
+from lib2to3.pgen2 import driver
+
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+
+
+# For W3C actions
+
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.actions import interaction
+from selenium.webdriver.common.actions.action_builder import ActionBuilder
+from selenium.webdriver.common.actions.pointer_input import PointerInput
+import pytest
+
+def testar_my_demo_app():
+    ambiente = 'saucelabs'
+
+
+    if ambiente == 'saucelabs':
+        caps = {
+            "platformName": "Android",
+            "appium:plafformVersion": "9.0",
+            "appium:deviceName": "Samsung Galaxy S9 FHD GoogleAPI Emulador",
+            "appium:deviceOrientation": "portrait",
+            "appium:app": "storage:filename=mda-1.0.13-15.apk",
+            "appium:appPackage": "com.saucelabs.mydemoapp.android",
+            "appium:appActivity": "com.saucelabs.mydemoapp.android.view.activities.SplashActiivity"
+        }
+
+
+        driver = webdriver.Remote(
+            "https://oauth-safiracamilo-72eee:17833994-ff6f-44e7-8a27-31500fbc3ba5@ondemand.eu-central-1.saucelabs.com:443/wd/hub",caps)  # pegar no site saucelebs em Driver Creation
+
+
+    else:
+        caps = {
+            "platformName": "Android",
+            "appium:plafformVersion": "9.0",
+            "appium:deviceName": "Samsung Galaxy S9 FHD GoogleAPI Emulador",
+            "appium:deviceOrientation": "portrait",
+            "appium:app": "storage:filename=mda-1.0.13-15.apk",
+            "appium:appPackage": "com.saucelabs.mydemoapp.android",
+            "appium:appActivity": "com.saucelabs.mydemoapp.android.view.activities.SplashActiivity"
+        }
+
+        driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", caps)
+
+    el1 = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Sauce Lab Back Packs")
+    el1.click()
+    el2 = driver.find_element(by=AppiumBy.ID, value="com.saucelabs.mydemoapp.android:id/productTV")
+    el2.click()
+    el3 = driver.find_element(by=AppiumBy.ID, value="com.saucelabs.mydemoapp.android:id/productTV")
+    el3.click()
+    el4 = driver.find_element(by=AppiumBy.ID, value="com.saucelabs.mydemoapp.android:id/priceTV")
+    el4.click()
+    actions = ActionChains(driver)
+    actions.w3c_actions = ActionBuilder(driver, mouse=PointerInput(interaction.POINTER_TOUCH, "touch"))
+    actions.w3c_actions.pointer_action.move_to_location(572, 309)
+    actions.w3c_actions.pointer_action.pointer_down()
+    actions.w3c_actions.pointer_action.move_to_location(661, 296)
+    actions.w3c_actions.pointer_action.release()
+    actions.perform()
+
+    actions = ActionChains(driver)
+    actions.w3c_actions = ActionBuilder(driver, mouse=PointerInput(interaction.POINTER_TOUCH, "touch"))
+    actions.w3c_actions.pointer_action.move_to_location(505, 422)
+    actions.w3c_actions.pointer_action.pointer_down()
+    actions.w3c_actions.pointer_action.move_to_location(532, 392)
+    actions.w3c_actions.pointer_action.release()
+    actions.perform()
+
+    actions = ActionChains(driver)
+    actions.w3c_actions = ActionBuilder(driver, mouse=PointerInput(interaction.POINTER_TOUCH, "touch"))
+    actions.w3c_actions.pointer_action.move_to_location(462, 362)
+    actions.w3c_actions.pointer_action.pointer_down()
+    actions.w3c_actions.pointer_action.move_to_location(226, 169)
+    actions.w3c_actions.pointer_action.release()
+    actions.perform()
+
+    el4.click()
+    el5 = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Gray color")
+    el5.click()
+    el6 = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Increase item quantity")
+    el6.click()
+    el7 = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Tap to add product to cart")
+    el7.click()
+    el8 = driver.find_element(by=AppiumBy.ID, value="com.saucelabs.mydemoapp.android:id/cartTV")
+    el8.click()
+    el9 = driver.find_element(by=AppiumBy.CLASS_NAME,
+                              value="//android.widget.RelativeLayout[@content-desc=\"Displays number of items in your cart\"]/android.widget.ImageView")
+    el9.click()
+    el10 = driver.find_element(by=AppiumBy.ID, value="com.saucelabs.mydemoapp.android:id/totalPriceTV")
+    el10.click()
+    el10.click()
+    driver.quit()
